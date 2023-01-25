70034a0e111614cb3b91b76caa6788233fd9eec3
tingaz-2nye <41738925+tingaz-2nye@users.noreply.github.com>
2023-01-25 16:34:06 +0200

    card feature

---
+++ src/components/Card.jsx
@@ -0,0 +1,126 @@
+import React, { useState } from 'react';
+import IMG from '../assets/0.jpeg';
+
+const Card = () => {
+  const [mouseOverState, setMouseOverState] = useState(false);
+  return (
+    <>
+      <div
+        className={`
+          ${
+            mouseOverState ? ' bg-rose-600 scale-100' : 'bg-purple-700 scale-95'
+          } transition ease-in-out duration-300 delay-75 w-60 h-2/3 shadow-md absolute z-0 transform  -rotate-6 rounded-lg
+        `}
+      ></div>
+      <div
+        className=" w-60 h-2/3 bg-slate-200 flex flex-col gap-2   shadow-md absolute z-10 rounded-lg"
+        onMouseOver={() => setMouseOverState(true)}
+        onMouseOut={() => setMouseOverState(false)}
+      >
+        <img
+          src={IMG}
+          className="w-24 h-24 rounded-full mx-auto mt-5 bg-slate-100"
+        ></img>
+        <h1 className="text-slate-800 text-lg font-thin uppercase mt-1 mx-auto">
+          Software Developer
+        </h1>
+        <h2 className=" text-slate-800 text-sm font-semibold uppercase mx-auto">
+          Tinga Warren Chingeni
+        </h2>
+
+        <div className="flex gap-3 justify-start ml-5 mt-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
+            />
+          </svg>
+          <span>GMT3</span>
+        </div>
+        <div className="flex gap-3 ml-5 mt-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+            />
+          </svg>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+            />
+          </svg>
+          <span>+265996256117</span>
+        </div>
+        <div className="flex gap-3 ml-5 mt-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25"
+            />
+          </svg>
+          <span>tinga413@gmail.com</span>
+        </div>
+        <div className="flex gap-3 ml-5 mt-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+            />
+          </svg>
+          <span>Blantyre, Malawi</span>
+        </div>
+        <button className="bg-purple-700 hover:bg-rose-600 text-white font-bold py-2 px-full mx-4 mt-5 rounded-full">
+          Send Contact
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Card;
